aab3fd7ac0d23326cabbf5d34f64f2482ade0571
Brand <you@example.com>
2026-06-26 22:39:24 -0400

    Added Position struct that spawns an entity and displays its current position

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,3 +1,35 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, spawn_entity)
+        .add_systems(Update, current_position)
+        .run();
 }
+
+#[derive(Component)]
+pub struct Position 
+    {
+        pub x: f32,
+        pub y: f32,
+    }
+    pub fn spawn_entity(mut commands: Commands) {
+        commands.spawn((Name { name: "Player".to_string() },Position { x: 0.0, y: 0.0 }, PlayerControlled));
+        commands.spawn((Name { name: "NPC".to_string() },Position { x: 10.0, y: 10.0 }));
+        
+    }
+    pub fn current_position(query: Query<(&Position, &Name), With<PlayerControlled>>) {
+        for (position, name) in query.iter() {
+            println!("{} is at position ({}, {})", name.name, position.x, position.y);
+        }
+    }
+#[derive(Component)]
+pub struct Name
+    {
+        pub name: String,
+    }
+
+#[derive(Component)]
+pub struct PlayerControlled;
+
