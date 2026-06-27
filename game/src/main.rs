5cf24b65198791f6ad689f89e7ba27c362e716f4
Brand <you@example.com>
2026-06-27 07:51:29 -0400

    Added a MovementX struct/function that adds x to position.x to simulate movement

--- game/src/main.rs
+++ game/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, spawn_entity)
-        .add_systems(Update, current_position)
+        .add_systems(Update, (current_position, move_x))
         .run();
 }
 
@@ -15,11 +15,11 @@ pub struct Position
         pub y: f32,
     }
     pub fn spawn_entity(mut commands: Commands) {
-        commands.spawn((Name { name: "Player".to_string() },Position { x: 0.0, y: 0.0 }, PlayerControlled));
+        commands.spawn((Name { name: "Player".to_string() },Position { x: 0.0, y: 0.0 }, MovementX { speed_x: 0.1 },PlayerControlled));
         commands.spawn((Name { name: "NPC".to_string() },Position { x: 10.0, y: 10.0 }));
         
     }
-    pub fn current_position(query: Query<(&Position, &Name), With<PlayerControlled>>) {
+    pub fn current_position(query: Query<(&mut Position, &Name), With<PlayerControlled>>) {
         for (position, name) in query.iter() {
             println!("{} is at position ({}, {})", name.name, position.x, position.y);
         }
@@ -30,6 +30,16 @@ pub struct Name
         pub name: String,
     }
 
+#[derive(Component)]
+pub struct MovementX
+    {
+    pub speed_x: f32,
+    }
+    pub fn move_x(mut query: Query<(&mut Position, &MovementX)>, time: Res<Time>) {
+        for (mut position, movement) in query.iter_mut() {
+            position.x += movement.speed_x * time.delta_secs(); // Move the entity along the x-axis based on speed and elapsed time.
+        }
+    }
 #[derive(Component)]
 pub struct PlayerControlled;
 
